5788e6ab38d1cd394421df05298631eea1227968
seaotter6382 <157032614+seaotter6382@users.noreply.github.com>
2024-04-24 09:24:43 -0400

    Update rockpapersissors.py

--- python/rockpapersissors.py
+++ python/rockpapersissors.py
@@ -1,2 +1,13 @@
 import random
-# working on it
+import time
+
+print("chose rock (1), paper (2), sissors (3)")
+while True:
+  try:
+    word = int(input())
+  except ValueError as e:
+    print("enter a number not a letter")
+    continue
+  break
+  
+computerchose = random.randint(1, 3)
