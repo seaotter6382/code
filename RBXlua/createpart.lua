a946949d438183f5d263665464458c782aeb3461
seaotter6382 <157032614+seaotter6382@users.noreply.github.com>
2024-04-25 10:12:37 -0400

    Create createpart.lua

---
+++ RBXlua/createpart.lua
@@ -0,0 +1,7 @@
+local workspace = game.workspace
+
+local part = Instance.new("Part")
+part.Parent = workspace
+part.Size = Vector3.new(1, 1, 1)
+part.Position = Vector3.new(0, 10, 0)
+part.Anchored = false
